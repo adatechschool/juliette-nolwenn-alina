d9cdac58f200d0429506d45db05ab7511ce0d0bd
Juliette Baron <juliette.baron.33@gmail.com>
2021-03-24 17:04:26 +0100

    clean code & doc

--- educatedwomen/ad.js
+++ educatedwomen/ad.js
@@ -1,45 +1,47 @@
+// Animation des images des femmes
 var myIndex = 0;
-carousel();
+moveImages();
 
-function carousel() {
+function moveImages() {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
+    for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
-  }
+    }
   myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
+    if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 9000); // Change image every 2 seconds
+  setTimeout(moveImages, 9000); // Change image every 9 seconds
 }
 
-
+// Animation du texte : nom et bio
 var slideIndex = 0;
-slide();
+moveText();
 
-function slide() {
+function moveText() {
   var i;
   var x = document.getElementsByClassName("myText");
-  for (i = 0; i < x.length; i++) {
+    for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
-  }
+    }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}    
+    if (slideIndex > x.length) {slideIndex = 1}    
   x[slideIndex-1].style.display = "block";  
-  setTimeout(slide, 9000);    
+  setTimeout(moveText, 9000);    
 }
 
+// Animation du chapeau
 var hatIndex = 0;
 moveHat();
 
 function moveHat() {
   var i;
   var x = document.getElementsByClassName("hat");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
   hatIndex++;
-  if (hatIndex > x.length) {hatIndex = 1}    
+    if (hatIndex > x.length) {hatIndex = 1}    
   x[hatIndex-1].style.display = "block";  
   setTimeout(moveHat, 9000); // Change image every 2 seconds
 }
