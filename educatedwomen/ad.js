8031fd7a31f3d0b5ec8e7b239f39006f97aa6aa4
Juliette Baron <juliette.baron.33@gmail.com>
2021-03-25 15:11:00 +0100

    ISS version finale - avec image ISS

--- educatedwomen/ad.js
+++ educatedwomen/ad.js
@@ -6,11 +6,13 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
   myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
   setTimeout(carousel, 9000); // Change image every 2 seconds
 }
 
@@ -22,12 +24,14 @@ function slide() {
   var i;
   var x = document.getElementsByClassName("myText");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}    
-  x[slideIndex-1].style.display = "block";  
-  setTimeout(slide, 9000); // Change text every 9 seconds   
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(slide, 9000); // Change text every 9 seconds
 }
 
 // Animation du chapeau
@@ -38,10 +42,12 @@ function moveHat() {
   var i;
   var x = document.getElementsByClassName("hat");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
   hatIndex++;
-  if (hatIndex > x.length) {hatIndex = 1}    
-  x[hatIndex-1].style.display = "block";  
+  if (hatIndex > x.length) {
+    hatIndex = 1;
+  }
+  x[hatIndex - 1].style.display = "block";
   setTimeout(moveHat, 9000); // Move hat every 9 seconds
 }
